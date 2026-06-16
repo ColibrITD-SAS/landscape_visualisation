68ccb877e2e633eece950ab07725850105ca5e86
Henri <henri.de.boutray@colibritd.com>
2026-06-16 17:00:29 +0200

    version fix

--- landscape_tools/_version.py
+++ landscape_tools/_version.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 from setuptools_scm import get_version
 
-__version__ = get_version(relative_to=Path(__file__).parent.parent)
+__version__ = get_version(relative_to=Path(__file__).parent)
